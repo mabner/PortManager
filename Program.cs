4de8bfb411f01256424031b99c8e4fa9b36371aa
Marcos Leite <m.abner@live.co.uk>
2021-12-25 23:39:05 -0300

    Initial files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PortManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
