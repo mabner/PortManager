01436f870c391b420c38c1733b2688cef560fcee
Marcos Leite <m.abner@live.co.uk>
2021-12-26 00:05:19 -0300

    adds main menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace PortManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+
+		}
+
+		private static string Menu()
+		{
+			Console.WriteLine("1. Add Ports");
+			Console.WriteLine("2. Remove Ports");
+			Console.WriteLine("3. Restart Ports");
+			Console.WriteLine("4. List Ports");
+			Console.WriteLine("5. Exit");
+			Console.WriteLine("Choose an option: ");
+			string choice = Console.ReadLine();
+			return choice;
+		}
     }
 }
